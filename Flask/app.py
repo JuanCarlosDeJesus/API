ca018c8f98c5f640d6225d4e598343e6a76ca7fd
Juan Carlos De Jesus <carl.d3j3sus@gmail.com>
2023-08-08 00:10:35 +0800

    First app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,1 +1,27 @@
+from flask import Flask
 
+# create a flask object
+app = Flask(__name__)
+
+# create route decorator
+@app.route("/")
+def index():
+    return "Hello World!"
+
+# create /about decorator
+@app.route("/about")
+def about():
+    return "<h1 style='color: red'>About Page!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+"""
+setting up your environment:
+run:
+export FLASK_APP=<filename.py>
+run:
+export FLASK_ENV=development - or production
+"""
