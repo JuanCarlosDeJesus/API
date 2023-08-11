710c6f52bf162b4067f0a6348d53b28233d6d755
Juan Carlos De Jesus <carl.d3j3sus@gmail.com>
2023-08-12 00:04:44 +0800

    Added render templates

--- Flask/app/views.py
+++ Flask/app/views.py
@@ -1,11 +1,12 @@
 # from the app package (folder) we are importing the app Flask object we initialized from the __init__.py file
 from app import app
 
+from flask import render_template
 
 # create route decorator
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 # create /about decorator
 @app.route("/about")
